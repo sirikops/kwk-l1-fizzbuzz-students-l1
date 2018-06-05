9a5ef748b61a3a5cd3c9cfa9aad1eb86fa389395
sirikops <sirikops@hotmail.com>
2018-06-05 19:20:31 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(int)
+  if int %3 == 0
+    puts "Fizz"
+  end
+  if int %5 == 0
+    puts "Buzz"
+  end
+  if int %3 == 0 && int %5 == 0
+    puts "FizzBuzz"
+  end
+end
+fizzbuzz(21)
