58c952695229d0c863c4483b6be282f1c54e0417
sirikops <sirikops@hotmail.com>
2018-06-05 19:04:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int %3
+  if int %3 == 0
     puts "Fizz"
   end
-  if int %5
+  if int %5 == 0
     puts "Buzz"
   end
-  if int %3 && %5
+  if int %3 == 0 && int %5 == 0
     puts "FizzBuzz"
   end
 end
